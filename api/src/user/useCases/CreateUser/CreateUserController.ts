60fdc60770b344e8ff2ea68a20d236d82ee77235
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-16 17:05:19 -0300

    ✨ feat: user controllers

---
+++ api/src/user/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { IErrorMessage } from '@shared/index';
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+export default class CreateUserController {
+  constructor(private useCase: CreateUserUseCase) {}
+
+  public async handle(request: Request, response: Response) {
+    const { name, email, password, role } = request.body;
+
+    try {
+      const user = await this.useCase.execute({
+        name,
+        email,
+        password,
+        role,
+      });
+
+      return response.status(201).json(user);
+    } catch (error) {
+      IErrorMessage(response, error.message, 400);
+    }
+  }
+}
