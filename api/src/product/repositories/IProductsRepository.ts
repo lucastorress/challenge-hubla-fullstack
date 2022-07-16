b7bfa38b57d79af5ce2e282f75f461eb5949e0b6
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-16 15:46:12 -0300

    ✨ feat: entities and repositories of products

---
+++ api/src/product/repositories/IProductsRepository.ts
@@ -0,0 +1,8 @@
+import IRepository from '../../shared/IRepository';
+import { Product, ProductProps } from '../entities/Product';
+
+export default interface IProductRepository
+  extends IRepository<ProductProps, Product> {
+  findProductByProducer(producerId: string): Promise<Product | null>;
+  findProductByTitle(title: string): Promise<Product | null>;
+}
