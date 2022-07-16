f8c002403fa9837e44dff3dfa801c0679c511fd5
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-16 15:54:49 -0300

    ⚡ chore: add helmet and morgan
    Improve Express security and logging

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,9 +4,9 @@ import { router } from './routes';
 // import swaggerUi = require('swagger-ui-express');
 // import swaggerDocument = require('./docs/swagger_output.json');
 
-// import helmet from 'helmet';
+import helmet from 'helmet';
 import compression = require('compression');
-// import morgan = require('morgan');
+import morgan = require('morgan');
 
 import 'dotenv/config';
 
@@ -14,9 +14,9 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(helmet());
+app.use(helmet());
 app.use(compression());
-// app.use(morgan('tiny'));
+app.use(morgan('tiny'));
 
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/v1', router);
