52124a5fd359998859b348c2737a84600df83a8d
Lucas P Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-17 23:58:14 -0300

    test: initial tests

---
+++ api/src/user/useCases/DeleteUser/__tests__/DeleteUserUseCase.spec.ts
@@ -0,0 +1,36 @@
+import InMemoryUsersRepositoryInstance from '@user/repositories/in-memory/InMemoryUsersRepository';
+import IUserRepository from '@user/repositories/IUsersRepository';
+import { DeleteUserUseCase } from '../DeleteUserUseCase';
+import { CreateUserUseCase } from '../../CreateUser/CreateUserUseCase';
+import { Roles } from '@user/entities/User';
+
+describe('Unit test: Delete User [Use Case]', () => {
+  let usersRepository: IUserRepository;
+  let deleteUserCase: DeleteUserUseCase;
+  let id: string;
+
+  beforeAll(async () => {
+    usersRepository = InMemoryUsersRepositoryInstance;
+    deleteUserCase = new DeleteUserUseCase(usersRepository);
+    const createUserCase = new CreateUserUseCase(usersRepository);
+    const mockUser = {
+      name: 'Username',
+      password: '123',
+      email: 'username@email.com',
+      role: Roles.AFFILIATE
+    }
+
+    const user = await createUserCase.execute(mockUser);
+    id = user.id;
+  });
+
+  it('should be able to delete an user that already exists', async () => {
+    const userDeleted = await deleteUserCase.execute({ id });
+    expect(userDeleted).toBe(true);
+  });
+
+  it('should not be able to delete an user that dont exists', async () => {
+    const userDeleted = await deleteUserCase.execute({ id });
+    expect(userDeleted).toBe(false);
+  });
+});
