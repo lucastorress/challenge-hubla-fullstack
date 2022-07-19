e8b8805551a1d5dc0002e9895e9b0d107a153e67
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-19 16:17:19 -0300

    ✨ feat: add routes for users, transactions and auth

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,16 +2,56 @@ import { Router } from 'express';
 import multer = require('multer');
 import { uploadTransactionBatchController } from './transaction';
 
-const router = Router();
+import { authenticateUserController, authMiddleware } from './auth';
 
+import {
+  addProducerToAffiliateController,
+  getUserController,
+  createUserController,
+  updateUserController,
+  deleteUserController,
+} from './user';
+
+/**
+ * We will use multer package to allow upload of files
+ */
 const storage = multer.memoryStorage();
 const upload = multer({
   storage,
   limits: { fileSize: 10000 },
 }).single('file');
 
+const router = Router();
+
+// Auth
+router.post('/auth/login', (request, response) => {
+  return authenticateUserController.handle(request, response);
+});
+
+// Transactions
 router.post('/transaction/upload', upload, (request, response) => {
   return uploadTransactionBatchController.handle(request, response);
 });
+
+// Users
+router.get('/user', authMiddleware, (request, response) => {
+  return getUserController.handle(request, response);
+});
+router.post('/user', (request, response) => {
+  return createUserController.handle(request, response);
+});
+router.put('/user', authMiddleware, (request, response) => {
+  return updateUserController.handle(request, response);
+});
+router.delete('/user', authMiddleware, (request, response) => {
+  return deleteUserController.handle(request, response);
+});
+router.post(
+  '/user/addProducerAffiliate',
+  authMiddleware,
+  (request, response) => {
+    return addProducerToAffiliateController.handle(request, response);
+  },
+);
 
 export { router };
