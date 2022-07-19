d395bef9adddfdf7e227a286dc1e2daab8fafcb3
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-19 18:04:13 -0300

    📖 docs: add swagger ui and documentation v1

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,8 @@
 import express = require('express');
 import { router } from './routes';
 
-// import swaggerUi = require('swagger-ui-express');
-// import swaggerDocument = require('./docs/swagger_output.json');
+import swaggerUi = require('swagger-ui-express');
+import swaggerDocument = require('./docs/swagger_output.json');
 
 import helmet from 'helmet';
 import compression = require('compression');
@@ -18,7 +18,7 @@ app.use(helmet());
 app.use(compression());
 app.use(morgan('tiny'));
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/v1', router);
 
 export { app };
