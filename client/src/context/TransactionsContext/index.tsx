a78c2f48d2e7dabd0c6117db88d2a1eea127ab54
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-20 10:44:43 -0300

    ✨ feat: add transaction context

---
+++ client/src/context/TransactionsContext/index.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useState, useContext, useMemo } from "react";
+import {
+  ClassifiedTransactionsByProduct,
+  FunctionalComponent,
+  Transactions,
+} from "@types";
+
+type TransactionsContextInterface = {
+  data: Transactions;
+  currentProduct: ClassifiedTransactionsByProduct;
+  handleChangeData: (data: Transactions) => void;
+  handleChangeCurrentProduct: (
+    product: ClassifiedTransactionsByProduct
+  ) => void;
+};
+
+const TransactionsContext = createContext({} as TransactionsContextInterface);
+
+export const useTransactions = (): TransactionsContextInterface =>
+  useContext(TransactionsContext);
+
+function TransactionsProvider({ children }: FunctionalComponent): JSX.Element {
+  const [transactions, setTransactions] = useState<Transactions>(
+    {} as Transactions
+  );
+  const [currentProduct, setCurrentProduct] =
+    useState<ClassifiedTransactionsByProduct>(
+      {} as ClassifiedTransactionsByProduct
+    );
+
+  const handleChangeCurrentProduct = (
+    product: ClassifiedTransactionsByProduct
+  ): void => {
+    setCurrentProduct(product);
+  };
+
+  const handleChangeData = (data: Transactions): void => {
+    setTransactions(data);
+  };
+
+  const value = useMemo(
+    () => ({
+      data: transactions,
+      handleChangeData,
+      currentProduct,
+      handleChangeCurrentProduct,
+    }),
+    [transactions, currentProduct]
+  );
+  return (
+    <TransactionsContext.Provider value={value}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
+
+export { TransactionsProvider, TransactionsContext };
