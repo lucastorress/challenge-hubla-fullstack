4da91755c9252c4fc167ce83b3dba55b6c4b7557
Lucas Pereira Torres de Araújo <lucast0rr3s@gmail.com>
2022-07-20 10:44:17 -0300

    ✨ feat: add types to be used in transaction upload

---
+++ client/src/@types/index.d.ts
@@ -0,0 +1,36 @@
+import { ReactNode } from "react";
+
+declare type FunctionalComponent = {
+  children: ReactNode;
+};
+
+type DetailsTransactionClassifiedByProduct = {
+  id: string;
+  type: number;
+  date: Date;
+  price: number;
+  seller: string;
+};
+
+export type ClassifiedTransactionsByProduct = {
+  productId: string;
+  productTitle: string;
+  details: DetailsTransactionClassifiedByProduct[];
+};
+
+type ClassifiedTransactionsByProducts = {
+  products: ClassifiedTransactionsByProduct[];
+};
+
+type TransactionValueByTypes = {
+  producerSales: number;
+  affiliateSales: number;
+  commissionPaid: number;
+  commissionReceived: number;
+};
+
+declare type Transactions = {
+  transactions: ClassifiedTransactionsByProducts;
+  totalValueByTypes: TransactionValueByTypes;
+  lengthOfTransactions: number;
+};
